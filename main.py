98fab8050ba3a544ec58b934610bf6d27bdc9e99
Damilare Agba <blankgodd33@gmail.com>
2021-12-04 23:36:04 +0100

    ok

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ app = FastAPI()
 try:
     sc = create_server_client()
 except:
-    sc = FaunaClient(secret=os.environ.get('FAUNA_SERVER_SECRET'))
+    sc = FaunaClient(secret='fnAEZmczZcACTLUwK1s910UUxFb4Zz3GePk31s2X')
+secret_key = "WZSqFa847EZhAuc9965"
 
 
 @app.post('/register')
@@ -53,68 +54,103 @@ def login(user: UserLogin):
             token = jwt.encode({
                 'email': resp['data']['email'],
                 'exp': datetime.utcnow() + timedelta(minutes=30)
-            }, os.environ.get('SECRET_KEY'), "HS256")
+            }, secret_key, "HS256")
             return {
                 "msg": "success",
                 "data": {
                     "token": token
                 }
             }
-    except:
-        pass
-    return {
-        "msg": "error",
-        "detail": "Email or password is incorrect"
-    }
+        else:
+            return {
+                "msg": "error",
+                "detail": "Email or password is incorrect"
+            }
+    except Exception as e:
+        return {
+            "msg": "error",
+            "detail": e
+        }
 
 
 # token required
 @app.get("/get-user")
 def get_user(x_access_token: str = Header(None)):
     try:
-        data = jwt.decode(x_access_token, os.environ.get('SECRET_KEY'),
+        data = jwt.decode(x_access_token, secret_key,
                           algorithms=['HS256'])
-        user = sc.query(q.get(q.match(q.index("users_by_email"), data['email'])))
-        user = user['data']
-        return {
-            "id": user["id"],
-            "email": user["email"],
-            "name": user["name"]
-        }
     except Exception:
         return {
-            'status': 'error',
-            'msg': 'Token is invalid'
+            'msg': 'error',
+            'details': 'Token is invalid'
         }
+    try:
+        user = sc.query(q.get(q.match(q.index("users_by_email"), data['email'])))
+    except:
+        return {
+            'msg': 'error',
+            'details': 'User not found'
+        }
+    user_id = user['ref'].id()
+    user = user['data']
+    return {
+        "id": user_id,
+        "email": user["email"],
+        "name": user["name"]
+    }
 
 
-@app.post("/update-user/{user_id}")
+@app.post("/update-user")
 def update_user(user: UpdateUser, x_access_token: str = Header(None)):
     try:
-        data = jwt.decode(x_access_token, os.environ.get('SECRET_KEY'),
+        data = jwt.decode(x_access_token, secret_key,
                           algorithms=['HS256'])
-        resp = sc.query(q.get(q.match(q.index("users_by_email"), data['email'])))
-        uid = resp['data']['id']
-        data = {}
-        if user.email is not None:
-            data["email"] = user.email
-        if user.name is not None:
-            data["name"] = user.name
-        if user.password is not None:
-            data["password"] = hash_password(user.password)
-
-        sc.query(q.replace(q.ref(q.collection("users"), uid), {"data": data}))
-        return {
-            "msg": "success",
-            "detail": "User updated successfully"
-        }
     except Exception:
         return {
-            'status': 'error',
-            'msg': 'Token is invalid'
+            'msg': 'error',
+            'details': 'Token is invalid'
         }
+    try:
+        resp = sc.query(q.get(q.match(q.index("users_by_email"), data['email'])))
+    except:
+        return {
+            'msg': 'error',
+            'details': 'User not found'
+        }
+    uid = resp['ref'].id()
+    data = {}
+    if user.email is not None:
+        data["email"] = user.email
+    if user.name is not None:
+        data["name"] = user.name
+    if user.password is not None:
+        data["password"] = hash_password(user.password)
+
+    # sc.query(q.replace(q.ref(q.collection("users"), uid), {"data": data}))
+    sc.query(q.update(q.ref(q.collection("users"), uid), {"data": data}))
+    return {
+        "msg": "success",
+        "details": "User updated successfully"
+    }
 
 
+@app.delete('/delete-user')
+def delete_user(x_access_token: str = Header(None)):
+    try:
+        data = jwt.decode(x_access_token, secret_key,
+                          algorithms=['HS256'])
+    except Exception:
+        return {
+            'msg': 'error',
+            'details': 'Token is invalid'
+        }
+    resp = sc.query(q.get(q.match(q.index("users_by_email"), data['email'])))
+    uid = resp['ref'].id()
+    sc.query(q.delete(q.ref(q.collection("users"), uid)))
+    return {
+        "msg": "success",
+        "details": "User deleted successfully"
+    }
 
 
 
